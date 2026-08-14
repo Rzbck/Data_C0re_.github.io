bf239fb06349031407278a3abb564b69a7594ce3
Data_C0re_ <10966796+Rzbck@users.noreply.github.com>
2026-08-14 15:01:46 +0200

    Activate secure contact form

--- assets/js/contact-config.js
+++ assets/js/contact-config.js
@@ -1,5 +1,5 @@
 window.DATA_C0RE_CONTACT = Object.freeze({
-  enabled: false,
-  endpoint: '',
-  turnstileSiteKey: ''
+  enabled: true,
+  endpoint: 'https://script.google.com/macros/s/AKfycbzwHiCy5xlMh6gi_Bk86tosOF5FxhLRTy4J02gx1ogVxr1iZrlHfXQHAo2aDGWYoTMK/exec',
+  turnstileSiteKey: '0x4AAAAAAEP1hdvcsC7mdnjh'
 });
